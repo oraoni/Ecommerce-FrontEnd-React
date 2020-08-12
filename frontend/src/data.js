0659bf9c4e26f526de022428a368e8a5f9805aad
oraoni <oraoni@gmail.com>
2020-08-12 14:29:24 -0700

    React Refactoring
    App now runs in react and has dynamic features.

---
+++ frontend/src/data.js
@@ -0,0 +1,34 @@
+export default {
+    products: [{
+        name: '1000 Gold',
+        image: '/images/goldchest1.jpg',
+        price: 19,
+        brand: 'Ganker',
+        rating: 4.9,
+        numReviews: 10
+    },
+    {
+        name: '1000 Gold',
+        image: '/images/goldchest1.jpg',
+        price: 25,
+        brand: 'Suricat',
+        rating: 4.9,
+        numReviews: 23123
+    },
+    {
+        name: '1000 Gold',
+        image: '/images/goldchest1.jpg',
+        price: 27,
+        brand: 'Erick',
+        rating: 5,
+        numReviews: 1113
+    },
+    {
+        name: '1000 Gold',
+        image: '/images/goldchest1.jpg',
+        price: 19,
+        brand: 'Panda',
+        rating: 5,
+        numReviews: 1923
+    }]
+}
